3627a54bcdb8893af934a65fd0293b80c4ac8d49
tamires-galvao <tamiresmorenogalvao@gmail.com>
2025-05-22 10:21:31 -0300

    Perfil no cabeçalho

--- src/pages/auth/sing-up.tsx
+++ src/pages/auth/sing-up.tsx
@@ -7,6 +7,8 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
+import { registerRestaurant } from '@/api/register-restautant'
 
 const signUpForm = z.object({
   restaurantName: z.string(),
@@ -24,18 +26,25 @@ export function SignUp() {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignUpForm>()
+  } = useForm<SignUpForm>() 
+
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  }) 
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
 
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+         onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
     } catch (error) {
