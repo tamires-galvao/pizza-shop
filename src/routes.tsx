2a309adbc78186d3e2a06010db3517a71e4c30eb
tamires-galvao <tamiresmorenogalvao@gmail.com>
2025-05-28 20:47:56 -0300

    Erros e pagina 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,17 +2,19 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sing-up'
-import { NotFound } from './pages/404'
+import { SignUp } from './pages/auth/sign-up'
+
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -37,5 +39,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
